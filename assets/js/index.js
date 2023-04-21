87cb9ea9f2df49b84af45202c8bde4e21aa61745
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-04-21 11:23:14 +0000

    Button on main page has different hover js

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+function mouseOver() {
+    document.getElementById('circle-one').style.backgroundColor = "blue";
+    document.getElementById('circle-two').style.backgroundColor = "blue";
+    document.getElementById('circle-three').style.backgroundColor = "blue";
+    document.getElementById('circle-four').style.backgroundColor = "blue";
+}
+
+function mouseOut() {
+    document.getElementById('circle-one').style.backgroundColor = "#9CFFFA";
+    document.getElementById('circle-two').style.backgroundColor = "#9CFFFA";
+    document.getElementById('circle-three').style.backgroundColor = "#9CFFFA";
+    document.getElementById('circle-four').style.backgroundColor = "#9CFFFA";
+}
