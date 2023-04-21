a65b4460d6075207ac5dd00e9065beb6e6dd8b56
ryanmcnally93 <ryanmcnallymusic@outlook.com>
2023-04-21 13:37:21 +0000

    Colour update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,13 @@
 function mouseOver() {
-    document.getElementById('circle-one').style.backgroundColor = "blue";
-    document.getElementById('circle-two').style.backgroundColor = "blue";
-    document.getElementById('circle-three').style.backgroundColor = "blue";
-    document.getElementById('circle-four').style.backgroundColor = "blue";
+    document.getElementById('circle-one').style.backgroundColor = "#813405";
+    document.getElementById('circle-two').style.backgroundColor = "#813405";
+    document.getElementById('circle-three').style.backgroundColor = "#813405";
+    document.getElementById('circle-four').style.backgroundColor = "#813405";
 }
 
 function mouseOut() {
-    document.getElementById('circle-one').style.backgroundColor = "#9CFFFA";
-    document.getElementById('circle-two').style.backgroundColor = "#9CFFFA";
-    document.getElementById('circle-three').style.backgroundColor = "#9CFFFA";
-    document.getElementById('circle-four').style.backgroundColor = "#9CFFFA";
+    document.getElementById('circle-one').style.backgroundColor = "#9ED0E6";
+    document.getElementById('circle-two').style.backgroundColor = "#9ED0E6";
+    document.getElementById('circle-three').style.backgroundColor = "#9ED0E6";
+    document.getElementById('circle-four').style.backgroundColor = "#9ED0E6";
 }
